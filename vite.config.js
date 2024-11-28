d3465a81cb8879e103d8670b22686b29252e8fcb
mee96 <carmenmc96@gmail.com>
2024-11-28 21:18:27 +0100

    pages

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/modulo-3-evaluacion-final-mee96/",
   server: { watch: { usePolling: true } }
-})
+});

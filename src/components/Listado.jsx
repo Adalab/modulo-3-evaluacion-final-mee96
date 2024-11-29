17b0ca9f9eb119970a78791e6055c7b803f29a3d
mee96 <carmenmc96@gmail.com>
2024-11-29 08:37:23 +0100

    link funciona

--- src/components/Listado.jsx
+++ src/components/Listado.jsx
@@ -1,27 +1,3 @@
-/*import React from 'react'
-import Personaje from './Personaje'
-import "../scss/layout/Listado.scss";
-
-function Listado({characters}) {
-  return (
-    <section>
-        <ul className='personaje'> 
-        {
-            characters.map((data) => {
-                return(
-                   <Personaje key={data.id} personajeData={data}/>
-                        //map demana una key cuan pintes al HTML solament
-                )
-            })
-        }
-        </ul>
-    </section>
-  )
-}
-
-export default Listado
-*/
-
 import React from 'react'
 import Personaje from './Personaje'
 import "../scss/layout/Listado.scss";
@@ -34,15 +10,15 @@ function Listado({ characters, filterName }) {
           No hay ningún personaje que coincida con "{filterName}".
         </p>
       ) : (
-        <ul className="personaje">
-          {characters.map((data) => (
-            <li key={data.id}>
-          
-              <img src={data.image} alt={data.name} />
-              <h3>{data.name}</h3>
-              <p>{data.species}</p>
-            </li>
-          ))}
+        <ul className='personaje'> 
+        {
+            characters.map((data) => {
+                return(
+                   <Personaje key={data.id} personajeData={data}/>
+                        //map demana una key cuan pintes al HTML solament
+                )
+            })
+        }
         </ul>
       )}
     </section>

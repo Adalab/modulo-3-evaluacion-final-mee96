802e40f5805b03a203f6738d8e73228f9c435553
mee96 <carmenmc96@gmail.com>
2024-11-29 08:54:22 +0100

    boton volver

--- src/components/Detalles.jsx
+++ src/components/Detalles.jsx
@@ -4,36 +4,47 @@ Quan fem clic en una targeta, apareixerà a pantalla completa amb més detalls s
 */
 
 
-import Portal from '/images/adventure-time-portal.gif'
+import Portal from '/images/adventure-time-portal.gif';
+import { Link } from "react-router-dom"; // Importar Link
 import "../scss/layout/Detalles.scss";
 
+function Detalles({ characterID }) {
+  console.log(characterID);
 
-function Detalles({characterID}) {
-    console.log(characterID)
-    
   return (
-    <section className='details'>
+    <section className="details">
 
-        <div> 
-            <img className='portal' src={Portal} alt="Gif del Portal" />
-        </div>
+      <div>
+        <img className="portal" src={Portal} alt="Gif del Portal" />
+      </div>
 
-        <div> 
-            <img src={characterID.image} alt="foto del personaje" />
-            <h3>{characterID.name} - {characterID.species}</h3>
-            <p>Current status: {characterID.status}</p>
-            <p>Born in: {characterID.origin.name}</p>
-            <p>Appears in {characterID.episode.length} episodes</p>
+      {characterID ? (
+        <div>
+          <img src={characterID.image} alt={`Foto de ${characterID.name}`} />
+          <h3>{characterID.name} - {characterID.species}</h3>
+          <p>Current status: {characterID.status}</p>
+          <p>Born in: {characterID.origin.name}</p>
+          <p>Appears in {characterID.episode.length} episodes</p>
+           <div className="back-link">
+            <Link  to="/">Volver</Link>
+          </div>
         </div>
-         <div> 
-            <img className='portal' src={Portal} alt="Gif del Portal" />
-        </div>
+      ) : (
+        <p>Personaje no encontrado.</p>
+      )}
+
+      <div>
+        <img className="portal" src={Portal} alt="Gif del Portal" />
+      </div>
+
+      
+     
 
     </section>
-  )
+  );
 }
 
-export default Detalles
+export default Detalles;
 
 
 
